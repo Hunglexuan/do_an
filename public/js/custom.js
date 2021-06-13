a83f6537f6754a5bc7b54d2f6bd0277befeec294
Hùng <voiconkiemtien@gmail.com>
2021-06-13 23:36:32 +0700

    update

--- public/js/custom.js
+++ public/js/custom.js
@@ -150,63 +150,6 @@ Version: 1.0
     });
   }
 
-  function validateEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-  }
-  function check(event) {
-    event.preventDefault();
-
-
-    var email = document.forms["myForm"]["email"].value;
-    var email = document.forms["myForm"]["password"].value;
-
-    if (email == "" || email == null || !validateEmail(email)
-      || password == "" || password == null) {
-      event.preventDefault();
-      $("#fail").fadeTo(5000, 500).slideUp(500, function () {
-        $("#fail").slideUp(500);
-
-      });
-
-    }
-    else {
-      // event.preventDefault();
-
-      $("#success").fadeTo(5000, 500).slideUp(500, function () {
-        $("#success").slideUp(500);
-
-      })
-
-      if (screen.width < 1024) {
-        formData = {
-          'email': email,
-          'password': password
-        }
-      }
-      else {
-        formData = {
-          'email': email,
-          'password': password
-        }
-      }
-      loadForm(formData);
-
-
-    }
-  }
-  function loadForm(formData) {
-    $.ajax({
-      type: "POST",
-      url: "/api/auth/login",
-      data: formData,
-      success: function () {
-        setTimeout(() => {
-          window.location.href = '/';
-        }, 2500)
-      }
-    });
-  }
 
 
   // Tooltip
