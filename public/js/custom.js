43f2ce19b08cedd7d6b116e5fbcdfcb8e86b7d3d
Hunglexuan <hunglxse05917@fpt.edu.vn>
2021-06-13 23:13:54 +0700

    update

--- public/js/custom.js
+++ public/js/custom.js
@@ -149,6 +149,11 @@ Version: 1.0
       autoplayHoverPause: true,
     });
   }
+
+  function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
   function check(event) {
     event.preventDefault();
 
