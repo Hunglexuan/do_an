1cdcb260567275337b27bcbb76af930e8fc29777
DESKTOP-PM9SDLD\Xi Quao <Hungvnhe130502@fpt.edu.vn>
2021-06-14 09:35:38 +0700

    Update 4 màn: LoginUser, ForgotPasswordUser, RegisterUser, MyProfileUser

--- src/models/core/index.js
+++ src/models/core/index.js
@@ -1,16 +1,27 @@
-
-export { default as Customer } from './Customer';
-export { default as Users } from './Users';
-export { default as Role } from './Role';
-export { default as Bill } from './Bill';
-export { default as BillProduct } from './BillProduct';
-export { default as Category } from './Category';
-export { default as Comment } from './Comment';
-export { default as Feedback } from './Feedback';
-export { default as Image } from './Image';
-export { default as Product } from './Product';
-export { default as Report } from './Report';
-export { default as Voucher } from './Voucher';
+export { default as Customer }
+from './Customer';
+export { default as Users }
+from './Users';
+export { default as Role }
+from './Role';
+export { default as Bill }
+from './Bill';
+export { default as BillProduct }
+from './BillProduct';
+export { default as Category }
+from './Category';
+export { default as Comment }
+from './Comment';
+export { default as Feedback }
+from './Feedback';
+export { default as Image }
+from './Image';
+export { default as Product }
+from './Product';
+export { default as Report }
+from './Report';
+export { default as Voucher }
+from './Voucher';
 
 
 import { sequelize } from '../../connections';
