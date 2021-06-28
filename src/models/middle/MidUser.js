4a695cb748e6e9d35467c7e6f52ffec430b66075
Hunglexuan <hunglxse05917@fpt.edu.vn>
2021-06-28 22:38:11 +0700

    update

--- src/models/middle/MidUser.js
+++ src/models/middle/MidUser.js
@@ -12,17 +12,6 @@ import { password } from "../../config/database";
 import { name } from "ejs";
 import { find } from "lodash";
 
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'hung',
-    password: '1234',
-    database: 'doan'
-});
-
-
-
-
 
 class MidUser {
     async getUserByEmail(email) {
