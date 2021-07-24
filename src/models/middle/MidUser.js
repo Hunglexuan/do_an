cb25c985c8f79288445ebf2d855e326b84e3bea7
Hunglexuan <hunglxse05917@fpt.edu.vn>
2021-07-24 16:50:43 +0700

    update

--- src/models/middle/MidUser.js
+++ src/models/middle/MidUser.js
@@ -3,18 +3,6 @@ import { Op } from "sequelize";
 import { checkPassword, hashPassword } from "../../libs/encrypt";
 import { generateToken } from "../../libs/token";
 import { ERROR_MESSAGE } from "../../config/error";
-import {
-    sendMailActiveOrder,
-    sendMailForgotPassword,
-} from "../../libs/sendmail";
-import { v4 as uuidv4 } from "uuid";
-import { password } from "../../config/database";
-import { name } from "ejs";
-import { find } from "lodash";
-
-
-
-
 
 
 
@@ -123,6 +111,7 @@ class MidUser {
         const token = await generateToken({ userid: userData.id, email: email });
         return {
             token,
+            userid: userData.id,
         };
     }
 
